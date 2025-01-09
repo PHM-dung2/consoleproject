0664839c61ada3fe8cbff1eca2f87315835a0dc1
magnoria <sidaumv@gmail.com>
2025-01-09 17:15:13 +0900

    커밋푸시전 저장

--- delivery_service/src/model/dto/EntryDto.java
+++ delivery_service/src/model/dto/EntryDto.java
@@ -1,80 +1,7 @@
 package model.dto;
 
 public class EntryDto {
-<<<<<<< HEAD
-	
 
-	private int eno;
-	private String ename;
-	private String espot;
-	private int etype;
-	private int mno;
-	
-	// 생성자
-	// 사용할것만 정리해 놓음
-	public EntryDto() {}
-	
-	public EntryDto(String ename , String espot , int etype, int mno) {
-		super();
-		
-		this.ename = ename;
-		this.espot = espot;
-		this.etype = etype;
-		this.mno = mno;
-		
-		
-		
-		
-		
-		
-		
-	}// f end
-	
-	
-	//3. 메소드 , getter/ setter , toString() 
-	// 사용할것만 정리해놓음
-	
-	@Override
-	public String toString() {
-		return "EntryDto [ename=" + ename + ", espot=" + espot + ", etype=" + etype + ", mno=" + mno + "]";
-	}
-
-	public int getEno() {
-		return eno;
-	}
-
-	public void setEno(int eno) {
-		this.eno = eno;
-	}
-
-	public String getEspot() {
-		return espot;
-	}
-
-	public void setEspot(String espot) {
-		this.espot = espot;
-	}
-
-	public int getEtype() {
-		return etype;
-	}
-
-	public void setEtype(int etype) {
-		this.etype = etype;
-	}
-
-	public int getMno() {
-		return mno;
-	}
-
-	public void setMno(int mno) {
-		this.mno = mno;
-	}
-	
-	
-	
-}// class end
-=======
 //	entry 테이블 멤버변수
 	private String ename;
 	private String espot;
@@ -203,14 +130,6 @@ public class EntryDto {
 	public void setLogInMno(String logInMno) {
 		this.logInMno = logInMno;
 	}
-	
 
-	
-	
-	
-
-	
-	
-	
 }
->>>>>>> branch 'parkheeman' of https://github.com/devcube2/deliveryService
+
