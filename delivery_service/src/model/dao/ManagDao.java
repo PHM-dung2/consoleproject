a25a718b5b787cc3c0c84e229ef248eb1f941f80
magnoria <sidaumv@gmail.com>
2025-01-10 18:13:15 +0900

    백업

---
+++ delivery_service/src/model/dao/ManagDao.java
@@ -0,0 +1,30 @@
+package model.dao;
+
+import java.net.ConnectException;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import model.dto.EntryDto;
+
+public class ManagDao {
+	// 싱글톤
+	private ManagDao() {}
+	private static ManagDao instance = new ManagDao();
+	public static ManagDao getInstance() {
+		return instance;
+	}
+		
+	//메소드
+	
+		
+		
+		
+	}//end
+	
+	
+	
+
+	
+
+
